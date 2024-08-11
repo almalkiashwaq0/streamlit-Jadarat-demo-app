ec2eae74682ebfe6f2257a4b0154a0c5c3c1d68f
Ashwaq <almalkiashwaq0@gmail.com>
2024-08-11 22:08:18 +0300

    Half

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.title("Hello, Streamlit!")
-st.write("This is a simple Streamlit app deployed on Community Cloud.")
-st.write("Hi")
+st.title("عندكم وظيفة؟")
+st.write("لوانت من جيل الطيبين بتسمع صوت الجمله")
+st.write("ولو انت مو منهم فالحمدلله انت لحقت عالتكنلوجيا ومو مضطر تلف كل مكان بملفك الاخضر تدور وظيفة")
+st.write("قد سمعت بجدارات؟")
+st.write("موقع طيب يلف ملفك وانت جالس في البيت بضغطة زر")
+st.write("لازم خبره؟ لا تقدر تقدم وبالعكس طالبين وظائف بدون خبره اكثر من بخبره واذا مو مصدقني شوف الشارت اللي تحت")
+st.image(r"C:\Users\almal\job.png")
